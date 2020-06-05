317ad60f3783f7853be5c961c7c724c56af175cf
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-06-05 11:02:51 +0300

    added modification

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,44 +1,21 @@
-console.log('it works!');
-// Prompt will show the fully quote you want to see while you are typing.
 
-const quote = prompt("Please add a word to make a full quote");
 
-//To set the var list of part sentences we use fonction with array.
+const quote1 = ["An ideas is salvation","by imagination","when I"];
+const quote2 = ["working", "on a problem", "I never think"];
+const quote3 = ["An ideas is salvation","by imagination","when I"];
 
-    var listWord ={ quote1:["An ideas is salvation","by imagination","when I"],
-quote2:["working", "on a problem", "I never think"],
-quote3:["big", "soft", "I mean"],
-quote4:["equal", "something", "rare"],
-quote5:["dig", "fun", "happy"]
 
-    }
-// Variable
+let combination = quote1[Math.floor(Math.random() * quote1.length)]+ '' + quote2[Math.floor(Math.random() * quote2.length)]+ '' + quote3[Math.floor(Math.random() * quote3.length)]+ '.';
+console.log(combination);
 
-var indexQuote = {
-  textOut: listWord
-};
 
- // Generate a random base on part of the sentences to return the ensemblance of all part sentences.
- 
- function generatePart() {
-  var combination = document.showQuote("valueImput").value;
-   var quoteIndex = '';
-  for ( var i = 0; i < combination.length; i++){
-    if (document.showQuote("quote").value=== 'textOut'){;
-    sub = indexQuote.textOut;
-     
-  }
-    // combination of the part sentences
-     quoteIndex += sub.quote1[Math.floor(Math.random()*sub.quote1.length)]+ '' + sub.quote2[Math.floor(Math.random()*sub.quote2.length)]+ '' + sub.quote3[Math.floor(Math.random()*sub.quote3.length)]+ '' + sub.quote4[Math.floor(Math.random()*sub.quote4.length)]+ '' + sub.quote5[Math.floor(Math.random()*sub.quote5.length)]+ '.' ;
-  }
-  return " Here quote:" + quoteIndex;
-}
 
-  function quoteCopleat(){
-    var quoteCom = document.showQuote("rare");
-    quoteCom.innertext = generatePart()
-  }
-  quoteCopleat();
+
+
+
+
+    
+
 
 
 
